17a808241656481ee53a68724fd7503e546f7ea7
Bendik August Nesbø <bendik.nesbo@gmail.com>
2017-08-29 09:39:41 +0200

    uncommited changes

--- Domain/Configuration/Properties.cs
+++ Domain/Configuration/Properties.cs
@@ -1,5 +1,5 @@
 ﻿namespace Domain.Configuration {
     public class SpecificLevelProperties {
-        public static string SpecificLevelPath = @"LevelImages\HardGallery2\Large06.png";
+        public static string SpecificLevelPath = @"LevelImages\Bonus1Gallery2\XLarge15.png";
     }
 }
--- DomainTests/SolverTests/SeasonImageSolverTests.cs
+++ DomainTests/SolverTests/SeasonImageSolverTests.cs
@@ -3,7 +3,7 @@ using DomainTests.TestBases;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DomainTests.SolverTests {
-    [TestClass]
+    //[TestClass]
     public class SeasonImageSolverTests : ImageSolverTests {
         private const int ImagesPerYear = 52;
         private const int ImagesPerWeek = 9;
@@ -18,7 +18,7 @@ namespace DomainTests.SolverTests {
         [TestMethod]
         public void All_Seaons_Combined() {
             Levels = LevelFactory.GetAll_Seasons_Combined();
-            Assert.AreEqual(ImagesPerWeek * WholeYearsAdded, Levels.Count);
+            Assert.AreEqual(ImagesPerYear * WholeYearsAdded, Levels.Count);
             Run();
         }
     }
